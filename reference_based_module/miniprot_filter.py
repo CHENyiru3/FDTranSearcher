6625ccc61cab41622e767f30cb92080e917d6475
CHENyiru3 <1556613010@qq.com>
2024-12-08 21:38:57 +0800

    update

--- reference_based_module/miniprot_filter.py
+++ reference_based_module/miniprot_filter.py
@@ -13,27 +13,22 @@ import os
 
 from GFFEntry_class import GFFEntry, parse_gff_attributes
 
-import os
-import subprocess
-from dataclasses import dataclass
-from typing import List, Dict, Tuple
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Blast import NCBIXML
-from Bio.Blast.Applications import NcbiblastnCommandline, NcbimakeblastdbCommandline
-import tempfile
-import os
-
-
-from GFFEntry_class import GFFEntry, parse_gff_attributes
-
-# This section is the implementation of the miniprot analysis, to identify protein region (transposase)
 def run_miniprot_analysis(
     miniprot_path: str,
     Tpase: str,
     transposable_elements: List[SeqRecord],
-    threads: int = 16
+    threads: int = 16,
+    c: int = 50000,
+    m: int = 10,
+    p: float = 0.2,
+    N: int = 200,
+    O: int = 3,
+    J: int = 8,
+    F: int = 8,
+    K: str = "5M",
+    outn: int = 5000,
+    outs: float = 0.5,
+    outc: float = 0.03
 ) -> List[GFFEntry]:
     """
     Run the miniprot analysis to identify protein regions in the transposable elements.
@@ -42,8 +37,8 @@ def run_miniprot_analysis(
         miniprot_path (str): Path to the miniprot executable.
         Tpase (str): Path to the Tpase sequences file.
         transposable_elements (list): List of SeqRecord objects representing the transposable elements.
-        threads (int): Number of threads to use.
-    
+        Others: miniprot parameters, for more information see the miniprot documentation.
+  
     Returns:
         list: List of GFFEntry objects representing the protein regions.
     """
@@ -51,58 +46,73 @@ def run_miniprot_analysis(
         miniprot_results = []
 
         # Create a temporary FASTA file to store all transposable elements
-        with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.fasta') as all_te_fasta:
+        with tempfile.NamedTemporaryFile(
+            mode="w", delete=False, suffix=".fasta"
+        ) as all_te_fasta:
             SeqIO.write(transposable_elements, all_te_fasta, "fasta")
             all_te_fasta_path = all_te_fasta.name
 
         # Create a temporary file to store the miniprot output in GFF format
-        miniprot_output = tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.gff').name
+        miniprot_output = tempfile.NamedTemporaryFile(
+            mode="w", delete=False, suffix=".gff"
+        ).name
 
-        # run miniprot command, with default parameters
-        miniprot_cmd = f"{miniprot_path} -ut {threads} -c 50000 -m 10 -p 0.2 -N 200 -O 3 -J 8 -F 8 -K 5M --outn=5000 --outs=0.5 --outc=0.03 --gff {all_te_fasta_path} {Tpase} > {miniprot_output}"
-        result = subprocess.run(miniprot_cmd, shell=True, capture_output=True, text=True)
+        # run miniprot command, with customizable parameters
+        miniprot_cmd = (
+            f"{miniprot_path} -ut {threads} -c {c} -m {m} -p {p} -N {N} -O {O} "
+            f"-J {J} -F {F} -K {K} --outn={outn} --outs={outs} --outc={outc} "
+            f"--gff {all_te_fasta_path} {Tpase} > {miniprot_output}"
+        )
+        result = subprocess.run(
+            miniprot_cmd, shell=True, capture_output=True, text=True
+        )
 
         if result.returncode != 0:
             print(f"miniprot run with error: {result.returncode}")
+            print(f"Error message: {result.stderr}")
             return []
 
         # parse the miniprot output in GFF format
-        with open(miniprot_output, 'r') as miniprot_file:
+        with open(miniprot_output, "r") as miniprot_file:
             for line in miniprot_file:
-                if line.startswith('#'):
+                if line.startswith("#"):
                     continue
-                fields = line.strip().split('\t')
+                fields = line.strip().split("\t")
                 if len(fields) < 9:
                     continue
-                seqid, source, feature, start, end, score, strand, phase, attributes = fields
-                if feature == 'CDS':
+                seqid, source, feature, start, end, score, strand, phase, attributes = (
+                    fields
+                )
+                if feature == "CDS":
                     # Search for the corresponding transposable element record
-                    te_record = next((te for te in transposable_elements if te.id == seqid), None)
+                    te_record = next(
+                        (te for te in transposable_elements if te.id == seqid), None
+                    )
 
                     if te_record is None:
                         print(f"Warming: No TE record found for {seqid}")
                         continue
 
                     # Get the original BLAST match information
-                    blast_strand = te_record.annotations['blast_match']['strand']
+                    blast_strand = te_record.annotations["blast_match"]["strand"]
 
                     # If the BLAST strand is minus, reverse the strand
-                    if blast_strand == '-':
-                        if strand == '+':
-                            strand = '-'
-                        elif strand == '-':
-                            strand = '+'
+                    if blast_strand == "-":
+                        if strand == "+":
+                            strand = "-"
+                        elif strand == "-":
+                            strand = "+"
 
                     gff_entry = GFFEntry(
-                        seqid=seqid,  
-                        source='miniprot',
-                        type='protein_coding_region',
+                        seqid=seqid,
+                        source="miniprot",
+                        type="protein_coding_region",
                         start=int(start),
                         end=int(end),
-                        score=float(score) if score != '.' else 0.0,
-                        strand=strand, 
+                        score=float(score) if score != "." else 0.0,
+                        strand=strand,
                         phase=phase,
-                        attributes=parse_gff_attributes(attributes)
+                        attributes=parse_gff_attributes(attributes),
                     )
                     miniprot_results.append(gff_entry)
 
--- reference_based_module/reference_based_main.py
+++ reference_based_module/reference_based_main.py
@@ -30,15 +30,34 @@ def run_analysis(
     output_report_file: str = None,
     output_csv_file: str = None,
     output_gff3_file: str = None,
-    evalue_threshold: float = 1e-5,
-    identity_threshold: float = 60.0,
-    alignment_length_threshold: int = 30,
-    max_target_seqs: int = 1000,
-    max_hsps: int = 10,
+    blast_evalue_threshold: float = 1e-5,
+    blast_identity_threshold: float = 60.0,
+    blast_alignment_length_threshold: int = 30,
+    blast_max_target_seqs: int = 1000,
+    blast_max_hsps: int = 10,
     threads: int = 16,
     # additional optional parameters
-    extension = 3000, mini_size = 2000, max_size = 15000, pattern_size = 8, gap_size = 1500,
-    tir_size = 5, mismatch_allowed = 2, report_mode = 'all'
+    extension=3000,
+    mini_size=2000,
+    max_size=15000,
+    pattern_size=8,
+    gap_size=1500,
+    tir_size=5,
+    mismatch_allowed=2,
+    report_mode="all",
+    # Miniprot parameters
+    miniprot_threads: int = 16,
+    miniprot_c: int = 50000,
+    miniprot_m: int = 10,
+    miniprot_p: float = 0.2,
+    miniprot_N: int = 200,
+    miniprot_O: int = 3,
+    miniprot_J: int = 8,
+    miniprot_F: int = 8,
+    miniprot_K: str = "5M",
+    miniprot_outn: int = 5000,
+    miniprot_outs: float = 0.5,
+    miniprot_outc: float = 0.03,
 ):
     try:
         # First step: use blastn to search for transposable elements
@@ -46,11 +65,11 @@ def run_analysis(
         gff_entries = run_blastn_search(
             target_sequence_file,
             genome_fasta_path,
-            evalue_threshold,
-            identity_threshold,
-            alignment_length_threshold,
-            max_target_seqs,
-            max_hsps,
+            blast_evalue_threshold,
+            blast_identity_threshold,
+            blast_alignment_length_threshold,
+            blast_max_target_seqs,
+            blast_max_hsps,
             threads,
         )
 
@@ -71,7 +90,18 @@ def run_analysis(
             miniprot_path,
             Tpase,
             transposable_elements,
-            threads
+            miniprot_threads,
+            miniprot_c,
+            miniprot_m,
+            miniprot_p,
+            miniprot_N,
+            miniprot_O,
+            miniprot_J,
+            miniprot_F,
+            miniprot_K,
+            miniprot_outn,
+            miniprot_outs,
+            miniprot_outc
         )
         print(f"miniprot analysis found {len(miniprot_results)} protein regions.")
 
@@ -84,17 +114,18 @@ def run_analysis(
             protein_regions = []
             for miniprot_entry in miniprot_results:
                 # Modify matching logic
-                if (miniprot_entry.seqid == te_record.id or
-                    te_record.id in miniprot_entry.seqid or
-                    miniprot_entry.seqid.split('_')[-1] in te_record.id):
+                if (
+                    miniprot_entry.seqid == te_record.id
+                    or te_record.id in miniprot_entry.seqid
+                    or miniprot_entry.seqid.split("_")[-1] in te_record.id
+                ):
                     protein_regions.append((miniprot_entry.start, miniprot_entry.end))
 
             # Only keep transposons that have found transposase proteins
             if protein_regions:
                 try:
                     structures = analyze_structures(
-                        str(te_record.seq),
-                        protein_regions
+                        str(te_record.seq), protein_regions
                     )
 
                     # Check if structures are empty
@@ -104,41 +135,45 @@ def run_analysis(
 
                     # Calculate the precise genomic position of the transposon structure
                     te_structure_start = min(
-                        structures[0]['TSD1'][0],
-                        structures[0]['TIR1'][0],
-                        structures[0]['TIR2'][0],
-                        structures[0]['TSD2'][0]
+                        structures[0]["TSD1"][0],
+                        structures[0]["TIR1"][0],
+                        structures[0]["TIR2"][0],
+                        structures[0]["TSD2"][0],
                     )
                     te_structure_end = max(
-                        structures[0]['TSD1'][1],
-                        structures[0]['TIR1'][1],
-                        structures[0]['TIR2'][1],
-                        structures[0]['TSD2'][1]
+                        structures[0]["TSD1"][1],
+                        structures[0]["TIR1"][1],
+                        structures[0]["TIR2"][1],
+                        structures[0]["TSD2"][1],
                     )
-                    architecture_score = structures[0]['scores']['total_score']
+                    architecture_score = structures[0]["scores"]["total_score"]
                     # Calculate the precise position in the reference genome
-                    blast_position = f"{blast_match['chromosome']}:{blast_match['start']}-{blast_match['end']}"
+                    blast_position = (
+                        f"{blast_match['chromosome']}:{blast_match['start']}-{blast_match['end']}"
+                    )
                     element_position = f"{blast_match['chromosome']}:{blast_match['start'] + te_structure_start}-{blast_match['start'] + te_structure_end}"
 
-                    analysis_results.append({
-                        'te_id': te_record.id,
-                        'blast_position': blast_position,
-                        'element_position': element_position,
-                        'blast_match': blast_match,
-                        'te_structure_position': {
-                            'start': te_structure_start,
-                            'end': te_structure_end
-                        },
-                        'protein_regions': protein_regions,
-                        'structures': structures,
-                        'source': 'FDT_reference_pipeline',
-                        'architecture_score': architecture_score,
-                        'type': 'functional_transposable_element',
-                        'strand': blast_match['strand'],  # Use BLAST's strand information
-                        'score': 0.0,
-                        'miniprot_strand': miniprot_entry.strand,  # Optionally record miniprot's strand in attributes
-                        'phase': '.',
-                    })
+                    analysis_results.append(
+                        {
+                            "te_id": te_record.id,
+                            "blast_position": blast_position,
+                            "element_position": element_position,
+                            "blast_match": blast_match,
+                            "te_structure_position": {
+                                "start": te_structure_start,
+                                "end": te_structure_end,
+                            },
+                            "protein_regions": protein_regions,
+                            "structures": structures,
+                            "source": "FDT_reference_pipeline",
+                            "architecture_score": architecture_score,
+                            "type": "functional_transposable_element",
+                            "strand": blast_match["strand"],  # Use BLAST's strand information
+                            "score": 0.0,
+                            "miniprot_strand": miniprot_entry.strand,  # Optionally record miniprot's strand in attributes
+                            "phase": ".",
+                        }
+                    )
 
                 except Exception as e:
                     print(f"Error processing TE {te_record.id}: {str(e)}")
@@ -148,7 +183,7 @@ def run_analysis(
 
         # Step 5: generate reports
         print("Step 5: Generating reports...")
-        if report_mode == 'all':
+        if report_mode == "all":
             generate_report(analysis_results, output_report_file)
             print(f"Detailed report saved to {output_report_file}.")
 
@@ -157,10 +192,10 @@ def run_analysis(
 
             generate_gff3_report(analysis_results, output_gff3_file)
             print(f"GFF3 report saved to {output_gff3_file}.")
-        elif report_mode == 'human_readable':
+        elif report_mode == "human_readable":
             generate_csv_report(analysis_results, output_csv_file)
             print(f"Summary CSV report saved to {output_csv_file}.")
-        elif report_mode == 'gff3':
+        elif report_mode == "gff3":
             generate_gff3_report(analysis_results, output_gff3_file)
             print(f"GFF3 report saved to {output_gff3_file}.")
         elif report_mode == "table":
@@ -170,48 +205,214 @@ def run_analysis(
         for file in [f for f in os.listdir() if f.endswith('.tmp')]:
             os.remove(file)
 
-def load_config(config_file):
-    """Load configuration from a YAML file."""
-    with open(config_file, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
-
-def merge_args_with_config(args, config):
-    """Override config dictionary with command-line arguments if provided."""
-    for key, value in vars(args).items():
-        if value is not None:  # Command-line argument takes precedence if provided
-            config[key] = value
-    return config
-import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="Reference-based Transposable Element Analysis")
+    parser = argparse.ArgumentParser(
+        description="Reference-based Transposable Element Analysis",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
 
-    # Add all arguments directly in the main function
-    parser.add_argument("-g", "--genome_fasta_path", required=True, help="Path to the genome FASTA file")
-    parser.add_argument("-ref", "--target_sequence_file", required=True, help="Path to the target sequence file")
-    parser.add_argument("-miniprot", "--miniprot_path", required=True, help="Path to the miniprot executable")
-    parser.add_argument("-tp", "--Tpase", help="Path to the hAT sequences file")
-    parser.add_argument("-or", "--output_report_file", help="Path to the output report file")
-    parser.add_argument("-ot", "--output_csv_file",  help="Path to the output CSV file")
-    parser.add_argument("-gff", "--output_gff3_file",  help="Path to the output GFF3 file")
-    parser.add_argument("--evalue_threshold", type=float, default=1e-5, help="E-value threshold for BLASTN")
-    parser.add_argument("--identity_threshold", type=float, default=60.0, help="Identity threshold for BLASTN")
-    parser.add_argument("--alignment_length_threshold", type=int, default=30, help="Alignment length threshold for BLASTN")
-    parser.add_argument("--max_target_seqs", type=int, default=1000, help="Maximum number of target sequences for BLASTN")
-    parser.add_argument("--max_hsps", type=int, default=10, help="Maximum number of HSPs for BLASTN")
-    parser.add_argument("--threads", type=int, default=16, help="Number of threads to use")
-    parser.add_argument("--pattern_size", type=int, default=8, help="Size of the TSD pattern")
-    parser.add_argument("--gap_size", type=int, default=1500, help="Maximum allowed gap between TSDs")
-    parser.add_argument("--tir_size", type=int, default=5, help="Size of the TIR pattern")
-    parser.add_argument("--mismatch_allowed", type=int, default=2, help="Number of allowed mismatches in TIRs")
-    parser.add_argument("--mini_size", type=int, default=2000, help="Minimum size of the structure")
-    parser.add_argument("--max_size", type=int, default=15000, help="Maximum size of the structure")
-    parser.add_argument("--report_mode", type=str, default="all", help="Report mode (all, human_readable, gff3, table)")
+    # Required arguments
+    parser.add_argument(
+        "-g",
+        "--genome_fasta_path",
+        required=True,
+        help="Path to the genome FASTA file (REQUIRED)",
+    )
+    parser.add_argument(
+        "-ref_dna",
+        "--target_sequence_file",
+        required=True,
+        help="Path to the target sequence file (reference TEs) (REQUIRED)",
+    )
+    parser.add_argument(
+        "-miniprot",
+        "--miniprot_path",
+        required=True,
+        help="Path to the miniprot executable (REQUIRED)",
+    )
+
+    # Optional arguments with default values
+    parser.add_argument(
+        "-ref_tp", "--Tpase", default=None, help="Path to the Tpase sequences file"
+    )
+    parser.add_argument(
+        "-or", "--output_report_file", help="Path to the output report file"
+    )
+    parser.add_argument(
+        "-ot", "--output_csv_file", help="Path to the output CSV file"
+    )
+    parser.add_argument(
+        "-gff", "--output_gff3_file", help="Path to the output GFF3 file"
+    )
+    parser.add_argument(
+        "--blast_evalue_threshold",
+        type=float,
+        default=1e-5,
+        help="E-value threshold for BLASTN (default: 1e-5)",
+    )
+    parser.add_argument(
+        "--blast_identity_threshold",
+        type=float,
+        default=60.0,
+        help="Identity threshold for BLASTN (default: 60.0)",
+    )
+    parser.add_argument(
+        "--blast_alignment_length_threshold",
+        type=int,
+        default=30,
+        help="Alignment length threshold for BLASTN (default: 30)",
+    )
+    parser.add_argument(
+        "--blast_max_target_seqs",
+        type=int,
+        default=1000,
+        help="Maximum number of target sequences for BLASTN (default: 1000)",
+    )
+    parser.add_argument(
+        "--blast_max_hsps",
+        type=int,
+        default=10,
+        help="Maximum number of HSPs for BLASTN (default: 10)",
+    )
+    parser.add_argument(
+        "--threads",
+        type=int,
+        default=16,
+        help="Number of threads to use (default: 16)",
+    )
+    parser.add_argument(
+        "--pattern_size",
+        type=int,
+        default=8,
+        help="Size of the TSD pattern (default: 8)",
+    )
+    parser.add_argument(
+        "--gap_size",
+        type=int,
+        default=1500,
+        help="Maximum allowed gap between TSDs (default: 1500)",
+    )
+    parser.add_argument(
+        "--tir_size",
+        type=int,
+        default=5,
+        help="Size of the TIR pattern (default: 5)",
+    )
+    parser.add_argument(
+        "--mismatch_allowed",
+        type=int,
+        default=2,
+        help="Number of allowed mismatches in TIRs (default: 2)",
+    )
+    parser.add_argument(
+        "--mini_size",
+        type=int,
+        default=2000,
+        help="Minimum size of the structure (default: 2000)",
+    )
+    parser.add_argument(
+        "--max_size",
+        type=int,
+        default=15000,
+        help="Maximum size of the structure (default: 15000)",
+    )
+    parser.add_argument(
+        "--report_mode",
+        type=str,
+        default="all",
+        choices=["all", "human_readable", "gff3", "table"],
+        help="""Report mode:
+        'all': Generate detailed report, CSV, and GFF3.
+        'human_readable': Generate only a summary CSV report.
+        'gff3': Generate only a GFF3 report.
+        'table': Generate only a detailed report.
+        (default: all)""",
+    )
+    parser.add_argument(
+        "--extension",
+        type=int,
+        default=3000,
+        help="Extension size for TE sequence extraction (default: 3000)",
+    )
+
+    # Miniprot specific arguments
+    parser.add_argument(
+        "--miniprot_threads",
+        type=int,
+        default=16,
+        help="Number of threads for miniprot (default: 16)",
+    )
+    parser.add_argument(
+        "--miniprot_c",
+        type=int,
+        default=50000,
+        help="Minimum spanning chain score for miniprot (default: 50000)",
+    )
+    parser.add_argument(
+        "--miniprot_m",
+        type=int,
+        default=10,
+        help="Minimal alignment score for miniprot (default: 10)",
+    )
+    parser.add_argument(
+        "--miniprot_p",
+        type=float,
+        default=0.2,
+        help="Minimal alignment score fraction for miniprot (default: 0.2)",
+    )
+    parser.add_argument(
+        "--miniprot_N",
+        type=int,
+        default=200,
+        help="Maximum number of intron hints for miniprot (default: 200)",
+    )
+    parser.add_argument(
+        "--miniprot_O",
+        type=int,
+        default=3,
+        help="Maximum number of introns for miniprot (default: 3)",
+    )
+    parser.add_argument(
+        "--miniprot_J",
+        type=int,
+        default=8,
+        help="Maximum intron length for miniprot (default: 8)",
+    )
+    parser.add_argument(
+        "--miniprot_F",
+        type=int,
+        default=8,
+        help="Maximum number of exons for miniprot (default: 8)",
+    )
+    parser.add_argument(
+        "--miniprot_K",
+        type=str,
+        default="5M",
+        help="Maximum memory to use for miniprot (default: 5M)",
+    )
+    parser.add_argument(
+        "--miniprot_outn",
+        type=int,
+        default=5000,
+        help="Maximum number of alignments to output for miniprot (default: 5000)",
+    )
+    parser.add_argument(
+        "--miniprot_outs",
+        type=float,
+        default=0.5,
+        help="Output score threshold for miniprot (default: 0.5)",
+    )
+    parser.add_argument(
+        "--miniprot_outc",
+        type=float,
+        default=0.03,
+        help="Output coverage threshold for miniprot (default: 0.03)",
+    )
 
     args = parser.parse_args()
 
-    # Now you can use args to access all the parsed arguments
+    # Call the run_analysis function with the parsed arguments
     run_analysis(
         genome_fasta_path=args.genome_fasta_path,
         target_sequence_file=args.target_sequence_file,
@@ -220,21 +421,35 @@ def main():
         output_report_file=args.output_report_file,
         output_csv_file=args.output_csv_file,
         output_gff3_file=args.output_gff3_file,
-        evalue_threshold=args.evalue_threshold,
-        identity_threshold=args.identity_threshold,
-        alignment_length_threshold=args.alignment_length_threshold,
-        max_target_seqs=args.max_target_seqs,
-        max_hsps=args.max_hsps,
-        threads=args.threads,
+        extension=args.extension,
+        mini_size=args.mini_size,
+        max_size=args.max_size,
         pattern_size=args.pattern_size,
         gap_size=args.gap_size,
         tir_size=args.tir_size,
         mismatch_allowed=args.mismatch_allowed,
-        mini_size=args.mini_size,
-        max_size=args.max_size,
-        report_mode=args.report_mode
-        
+        report_mode=args.report_mode,
+        miniprot_threads=args.miniprot_threads,
+        miniprot_c=args.miniprot_c,
+        miniprot_m=args.miniprot_m,
+        miniprot_p=args.miniprot_p,
+        miniprot_N=args.miniprot_N,
+        miniprot_O=args.miniprot_O,
+        miniprot_J=args.miniprot_J,
+        miniprot_F=args.miniprot_F,
+        miniprot_K=args.miniprot_K,
+        miniprot_outn=args.miniprot_outn,
+        miniprot_outs=args.miniprot_outs,
+        miniprot_outc=args.miniprot_outc,
+        blast_evalue_threshold=args.blast_evalue_threshold,
+        blast_identity_threshold=args.blast_identity_threshold,
+        blast_alignment_length_threshold=args.blast_alignment_length_threshold,
+        blast_max_target_seqs=args.blast_max_target_seqs,
+        blast_max_hsps=args.blast_max_hsps,
+        threads=args.threads
     )
 
 if __name__ == "__main__":
     main()
+
+
